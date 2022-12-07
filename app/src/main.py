18f48b430334ccf16568dcd72649bc3afd33761b
Jose Cruz <josedanielcrz@gmail.com>
2022-12-07 18:29:00 -0500

    Change data to show

--- app/src/main.py
+++ app/src/main.py
@@ -5,20 +5,9 @@ from infrastructure.db.db_connection import db_connection
 
 
 db = db_connection()
-dataframe = db.execute("SELECT * FROM REMATES")
+auctions = db.execute("SELECT * FROM REMATES").sort_values(by=['COD_DEP'])
 
-department = dataframe[["COD_DEP", "DEPARTAMENTO", "REGIONAL"]]
-department = department.drop_duplicates()
-department = department.sort_values(by=['COD_DEP'])
-department.rename(columns={'COD_DEP': 'COD_DEP', 'DEPARTAMENTO': 'DEPARTMENT', 'REGIONAL': 'REGION'}, inplace=True)
-
-town = dataframe[["COD_MUN", "MUNICIPIO", "COD_DEP"]]
-town = town.drop_duplicates()
-town = town.sort_values(by=['COD_MUN'])
-town.rename(columns={'COD_MUN': 'COD_TOWN', 'MUNICIPIO': 'TOWN', 'COD_DEP': 'COD_DEP'}, inplace=True)
-
-auction = dataframe[["PERIODO", "COD_MUN", "REMATES"]]
-auction = auction.drop_duplicates()
-auction = auction.sort_values(by=['PERIODO', 'REMATES'])
-auction.rename(columns={'PERIODO': 'PERIOD', 'COD_MUN': 'COD_TOWN', 'REMATES': 'AUCTIONS'}, inplace=True)
-auction.drop(auction[auction['AUCTIONS'] == 0].index, inplace=True)
+auctions = auctions[["REGIONAL", "DEPARTAMENTO", "MUNICIPIO", "PERIODO", "REMATES"]]
+auctions = auctions.drop_duplicates()
+auctions.rename(columns={'REGIONAL': 'REGION', 'DEPARTAMENTO': 'DEPARTMENT', 'MUNICIPIO': 'TOWN', 'PERIODO': 'PERIOD', 'REMATES': 'AUCTIONS'}, inplace=True)
+auctions.drop(auctions[auctions['AUCTIONS'] == 0].index, inplace=True)
